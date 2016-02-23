ebd41815ef6195a5678ca757fe8b84c6a0a4c62e
yanosz <yanosz@gmx.net>
2016-02-24 00:09:13 +0100

    Zwischenstand Unit-test

---
+++ tests/test_link_local_if.py
@@ -0,0 +1,7 @@
+from mkbgp import main
+def create_config(srcdir, exclude, prefix, defaulttemplate, templates, family,
+                  fmtclass, timeout):
+    pass
+
+def test_function():
+    assert main()
