a64db8df6a9fde65b0fec32217ee6d58e2e10296
yanosz <yanosz@gmx.net>
2016-03-01 16:20:41 +0100

    Zwischenstand

--- tests/test_link_local_if.py
+++ tests/test_link_local_if.py
@@ -1,7 +1,13 @@
-from mkbgp import main
+import mkbgp
+
 def create_config(srcdir, exclude, prefix, defaulttemplate, templates, family,
                   fmtclass, timeout):
     pass
 
 def test_function():
-    assert main()
+    print("running")
+    mkbgp.create_config = create_config
+    mkbgp.main()
+
+if __name__ == "__main__":
+    test_function()  
